bc0b0b1001f6674ac9a8cf71c2e604bf58547a17
Wade James Russell <wados.russell70@gmail.com>
2020-05-16 16:19:59 +1000

    Added BookDTO and get book response

---
+++ MyLibrary.Common/DTOs/BookDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Common.DTOs
+{
+    public class BookDTO
+    {
+        public int BookID { get; set; }
+        public string ISBN { get; set; }
+        public string eISBN { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public int? SeriesID { get; set; }
+        public int? NumberInSeries { get; set; }
+        public string Edition { get; set; }
+        public int PublicationFormat { get; set; }
+        public int FictionType { get; set; }
+        public int FormType { get; set; }
+        public int Publisher { get; set; }
+        public byte[] CoverImage { get; set; }
+    }
+}
---
+++ MyLibrary.Common/Responses/Book/GetBookResponse.cs
@@ -0,0 +1,12 @@
+﻿using MyLibrary.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Common.Responses
+{
+    class GetBookResponse : BaseResponse
+    {
+        public BookDTO Book { get; set; }
+    }
+}
