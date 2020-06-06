d4f32d172e99b2e82f8c1c7747ef268316ffd7dc
Wade James Russell <wados.russell70@gmail.com>
2020-06-06 12:28:40 +1000

    Added GetBooksResponse

---
+++ MyLibrary.Common/Responses/Book/GetBooksResponse.cs
@@ -0,0 +1,17 @@
+﻿using MyLibrary.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Common.Responses
+{
+    public class GetBooksResponse : BaseResponse
+    {
+        public List<BookDTO> Books { get; set; }
+
+        public GetBooksResponse()
+        {
+            Books = new List<BookDTO>();
+        }
+    }
+}
