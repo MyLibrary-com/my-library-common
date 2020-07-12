74a7ada721084da2417aa818fda9eea9646f5ae3
radulfr96 <wados.russell70@gmail.com>
2020-07-12 14:41:34 +1000

    removed unneeded namespace

--- MyLibrary.Common/Requests/Book/UpdateBookRequest.cs
+++ MyLibrary.Common/Requests/Book/UpdateBookRequest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace MyLibrary.Common.Requests.Book
+namespace MyLibrary.Common.Requests
 {
     public class UpdateBookRequest : BaseRequest
     {
